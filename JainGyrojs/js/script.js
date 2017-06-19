bdd7f51e57ed7b23d237c913bf2138f5008c514c
jainnn <jainlee2016@gmail.com>
2017-06-20 07:32:11 +0900

    Update script.js

--- JainGyrojs/js/script.js
+++ JainGyrojs/js/script.js
@@ -75,8 +75,8 @@ Bar.prototype = {
 			this.y = height-this.diam/2;
 		}
 
-	rect(this.x, this.y, this.diam * forceX*50, this.diam * forceY*50);			
-	rect(this.x+100, this.y+100, this.diam * forceX*40, this.diam * forceY*40);	
+	rect(this.x, this.y, this.diam * forceX*20, this.diam * forceY*40);			
+	rect(this.x+100, this.y+100, this.diam * forceX*30, this.diam * forceY*40);	
 	rect(this.x-30, this.y-50, this.diam * forceX*10, this.diam * forceY*10);
 	}
 }
